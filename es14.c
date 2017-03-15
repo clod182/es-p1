fdd3c68c361742c8349e966601634639cf9a261f
Claudio Manesso <clod182@hotmail.com>
2017-03-15 15:16:20 +0100

    Primi passi es1
    Rivedere puntatori

--- es14.c
+++ es14.c
@@ -1,13 +1,19 @@
+/*Manesso Claudio 859326*/
 #include <stdlib.h>
 #include <stdio.h>
 #include "es14.h"
 
 void riempi_scacchiera(char *fen, char campo[DIM][DIM]) {
-
+	int i=0,j=0;
+	for(i=0;i<DIM;i++){
+		for(j=0;j<DIM;j++){
+			campo[i][j]='0';
+		}
+	}
 }
 
 void vettore_casuale(int v[], int n, int min, int max) {
-
+	
 }
 
 int conta_sequenze(int n) {
@@ -15,9 +21,22 @@ int conta_sequenze(int n) {
 }
 
 /***********************MAIN*****************************/
+void stampa_matrice(char mat[DIM][DIM]){
+	int i=0,j=0;
+	for(i=0;i<DIM;i++){
+		for(j=0;j<DIM;j++){
+			printf("%c ",mat[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+}
 int main() {
-
-	/* Scrivete il vostro main qui. */
+	char *fen;
+	char campo[DIM][DIM];
+	stampa_matrice(campo);
+	riempi_scacchiera(fen,campo);
+	stampa_matrice(campo);
 
 	return EXIT_SUCCESS;
 }
