6abd6af4a4936fd592d1d518c0f7e7a23da31e57
Claudio Manesso_fisso <clod182@hotmail.com>
2017-03-16 17:55:59 +0100

    Riempimento matrice es1 da sistemare

--- es14.c
+++ es14.c
@@ -4,17 +4,27 @@
 #include "es14.h"
 
 void riempi_scacchiera(char *fen, char campo[DIM][DIM]) {/*------------------------------------------------------------------------------------------------es1-*/
-	int i=0,j=0;
+	int i=0,j=0,c=0;	
 	for(i=0;i<DIM;i++){/*inizializzo a 0 la matrice*/
 		for(j=0;j<DIM;j++){
-			campo[i][j]='0';
+			campo[i][j]=' ';
+		}
+	}	
+	for(i=0;i<DIM;i++){
+		for(j=0;j<DIM ;j++){		//&& fen[c]!='/'	
+				if((fen[c]>=65 && fen[c]<=90) || (fen[c]>=97 && fen[c]<=122)){
+					campo[i][j]=fen[c];
+				}
+				else{
+					j=j+(50-fen[c]);
+				}					
+			c++;
 		}
 	}
-	i=0;
-	while(&fen[i]){
-		printf("%c ",&fen[i]);
-		i++;
-	}	
+
+	
+		
+		
 }
 
 void vettore_casuale(int v[], int n, int min, int max) {/*------------------------------------------------------------------------------------------------es2-*/
@@ -39,11 +49,11 @@ void stampa_matrice(char mat[DIM][DIM]){
 
 
 int main() {
-	char parola[]="casa";
-	char *fen = &parola;
-	char campo[DIM][DIM];
-	stampa_matrice(campo);
-	riempi_scacchiera(*fen,campo);
+	char parola[]="r1bq1rk1/pp3ppp/3n4/2p1N3/2B5/7P/PPP2PP1/R1BQR1K1";	
+	char* npt;
+	npt=&parola;
+	char campo[DIM][DIM];	
+	riempi_scacchiera(&parola,campo);
 	stampa_matrice(campo);
 
 	return EXIT_SUCCESS;
