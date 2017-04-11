4d5ae52d27c99147208730c5a8f3b1a1b9ea91dd
Claudio Manesso_fisso <clod182@hotmail.com>
2017-04-11 07:05:12 +0200

    es 2 wip / es16

--- es16.c
+++ es16.c
@@ -42,13 +42,33 @@ int best_zain(struct oggetto v[], int n, int p, int idx,int best,int bestb){
 		bestb=best;
 		best=aus_zain(v,n,p,idx);
 		if(best>bestb){
-			return best_zain(v,n,p,idx+1,best,bestb);
+			//return best_zain(v,n,p,idx+1,best,bestb);
+			return best_zain(v+1,n,p,idx+1,best,bestb);
 		}
 		else{
 			best=bestb;
-			return best_zain(v,n,p,idx+1,best,bestb);
-		}
+			//return best_zain(v,n,p,idx+1,best,bestb);
+			return best_zain(v+1,n,p,idx+1,best,bestb);
+		}		
 	}
+}
+
+int aus_mon(struct moneta m[], int n, int somma, int i, int quanTot){
+	if(i==n){
+		return 0;
+	}
+	else if(m[i].quantita>0 && m[i].valore+quanTot<somma){
+		quanTot=quanTot+m[i].valore;
+		m[i].quantita--;
+		return aus_mon(m,  n,  somma,  i,  quanTot);
+	}
+	else if(m[i].quantita<=0 || m[i].valore+quanTot>somma){
+		return aus_mon(m, n, somma, i+1, quanTot);
+	}
+	else if(m[i].valore+quanTot==somma){
+		m[i].quantita--;
+		return aus_mon(m, n, somma, i, 0)+1;
+	}	
 }
 /*------------------------------------------------------------------------------------------------es1-*/
 int zaino(struct oggetto v[], int n, int p) {
@@ -56,5 +76,5 @@ int zaino(struct oggetto v[], int n, int p) {
 }
 /*------------------------------------------------------------------------------------------------es2-*/
 int combinazioni_monete(struct moneta m[], int n, int somma) {
-	return -1;
+	return aus_mon(m,n,somma,0,0);
 }
--- es16_main.c
+++ es16_main.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "es16.h"
-
+#include "es16.c"
 /*
  * Compilare con il comando `gcc -std=gnu89 -Wall -pedantic -o es16 es16.c es16_main.c`.
  * Aggiungere l'opzione `-ggdb` se volete utilizzare il debugger.
@@ -13,13 +13,21 @@ int main() {
 	struct oggetto v1[3]={{3, 5}, {8, 11}, {2, 3}};
 	struct oggetto v2[4]={{3, 5}, {8, 11}, {2, 3}, {5, 7}};
 	struct oggetto v3[4]={{3, 5}, {8, 11}, {2, 3}, {6, 7}};
+	struct oggetto v33[5]={{3, 5}, {8, 11}, {6, 7},{1,3},{2, 3}};
 	struct oggetto v4[5]={{3, 5}, {2, 80}, {2, 3}, {6, 20}, {6, 7}};
 	x=zaino(v1, 3, 8);
 	printf("-*-*-*-*-*-*-*-*-   es1    *-*-*-*-*-*-*-**\n");
 	printf("v1 == %d \n", x);
 	printf("v2 == %d \n", zaino(v2, 4, 10));
 	printf("v3 == %d \n", zaino(v3, 4, 10));
-	printf("v4 == %d \n", zaino(v4, 5, 10));
+	printf("v33 == %d \n", zaino(v33, 5, 10));
+	printf("v4 == %d \n", zaino(v4, 5, 8));
 	printf("-*-*-*-*-*-*-*-*-   es2    *-*-*-*-*-*-*-**\n");
+	struct moneta m1[3]={{5, 2}, {2, 3}, {1, 7}};
+	struct moneta m2[3]={{5, 2}, {2, 3}, {1, 7}};
+	struct moneta m3[3]={{5, 2}, {2, 2}, {1, 5}};
+	printf("m1 == %d \n", combinazioni_monete(m1, 3, 6));
+	printf("m2 == %d \n", combinazioni_monete(m2, 3, 7));
+	printf("m3 == %d \n", combinazioni_monete(m3, 3, 7));
 	return EXIT_SUCCESS;
 }
